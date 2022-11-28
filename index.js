6a2a6311f722c33d90e3f91f83e2aa682f4f2575
didarul381 <didarula128@gmail.com>
2022-11-28 03:22:20 -0800

    midleware added

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bje6fgv.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-onsole.log(process.env.ACCESS_TOKEN)
+// console.log(uri)
+// onsole.log(process.env.ACCESS_TOKEN)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
